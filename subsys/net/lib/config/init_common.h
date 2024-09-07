a88140573197d664b25a624aa215a2309bca7603
Florian Grandel <fgrandel@code-for-humans.de>
2024-09-07 20:56:47 +0200

    net: lib: config: exemplify macro config target
    This commit demonstrates how the macro target can be used to satisfy
    several important requirements:
    
    - scalability (config system immediately scales to all subsystems based
      on generic source parsing and target generation with zero subsys
      knowledge.
    - zero-footprint by default (config nodes that are not present or not
      enabled wil not consume any resources, structs can be optimized).
    - encapsulation (we are not forced to couple configuration files to the
      configuration model, many usage sites can make independent use of the
      same configuration source w/o unduely coupling them together)
    - normalization (all properties that functionally belong to the same
      entity are placed into that entity although they may come from
      distinct configuration files and configuration formats.
    - build-time validation of cross-references (faulty cross-references
      will be identified at build time and a sensible error message will be
      printed that helps users to fix the problem).
    - re-use of existing community investment: generic macros from the DT
      space can be re-used (and more importantly: the know-how that users
      have built around it).
    - self documenting via bindings (no need for manually synchronized
      documentation pages)
    
    Signed-off-by: Florian Grandel <fgrandel@code-for-humans.de>

---
+++ subsys/net/lib/config/init_common.h
@@ -0,0 +1,61 @@
+/* L3+ network settings header */
+
+/*
+ * Copyright (c) 2024 The Zephyr Project.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __NET_LIB_CONFIG_COMMON_H__
+#define __NET_LIB_CONFIG_COMMON_H__
+
+#include <stdbool.h>
+
+#include <zephyr/devicetree.h>
+#include <zephyr/device.h>
+
+#define NET_CONFIG_NET_DEVICE(iface_node_id) DT_PARENT(iface_node_id)
+#define NET_CONFIG_NET_DEVICE_ORD_NAME(iface_node_id)                                              \
+	DEVICE_DT_NAME_GET(NET_CONFIG_NET_DEVICE(iface_node_id))
+#define NET_CONFIG_NET_DEVICE_NAME(iface_node_id)                                                  \
+	DEVICE_DT_NAME(NET_CONFIG_NET_DEVICE(iface_node_id))
+
+#define NET_CONFIG_SUBNODE(_node_id, _subnode) DT_CHILD(_node_id, _subnode)
+
+#define NET_CONFIG_SUBNODE2(_node_id, _subnode, _subnode2)                                         \
+	DT_CHILD(DT_CHILD(_node_id, _subnode), _subnode2)
+
+#define NET_CONFIG_IF_SUBSTATE(_node_id, _subnode, _bracketed_then)                                \
+	IF_ENABLED(DT_NODE_HAS_STATUS(NET_CONFIG_SUBNODE(_node_id, _subnode), okay),               \
+		   _bracketed_then)
+
+#define NET_CONFIG_COND_SUBSTATE(_node_id, _subnode, _bracketed_then, _bracketed_else)             \
+	COND_CODE_1(DT_NODE_HAS_STATUS(NET_CONFIG_SUBNODE(_node_id, _subnode), okay),              \
+		    _bracketed_then, _bracketed_else)
+
+#define NET_CONFIG_SUBSTATE_NAME(_iface_node_id, _subnode)                                         \
+	CONCAT(NET_CONFIG_NET_DEVICE_ORD_NAME(_iface_node_id), _net_init_config_, _subnode)
+
+#define NET_CONFIG_SUBSTATE2_NAME(_iface_node_id, _subnode, _subnode2)                             \
+	CONCAT(NET_CONFIG_SUBSTATE_NAME(_iface_node_id, _subnode), _, _subnode2)
+
+#define NET_CONFIG_SUBSTATE_PTR_OR_NULL(_iface_node_id, _subnode)                                  \
+	NET_CONFIG_COND_SUBSTATE(_iface_node_id, _subnode,                                         \
+				 (&NET_CONFIG_SUBSTATE_NAME(_iface_node_id, _subnode)), (NULL))
+
+#define NET_CONFIG_SUBSTATE2_PTR_OR_NULL(_iface_node_id, _subnode, _subnode2)                      \
+	NET_CONFIG_COND_SUBSTATE(                                                                  \
+		NET_CONFIG_SUBNODE(_iface_node_id, _subnode), _subnode2,                           \
+		(&NET_CONFIG_SUBSTATE2_NAME(_iface_node_id, _subnode, _subnode2)), (NULL))
+
+#define NET_CONFIG_SUBSTATE2_OR_NULL(_iface_node_id, _subnode, _subnode2)                          \
+	NET_CONFIG_COND_SUBSTATE(NET_CONFIG_SUBNODE(_iface_node_id, _subnode), _subnode2,          \
+				 (NET_CONFIG_SUBSTATE2_NAME(_iface_node_id, _subnode, _subnode2)), \
+				 (NULL))
+
+#define NET_CONFIG_SUBSTATE_STR_ARRAY(_iface_node_id, _subnode, _str_array_field)                  \
+	static const char *NET_CONFIG_SUBSTATE2_NAME(_iface_node_id, _subnode,                     \
+						     _str_array_field)[] =                         \
+		DT_PROP(NET_CONFIG_SUBNODE(_iface_node_id, _subnode), _str_array_field)
+
+#endif /* __NET_LIB_CONFIG_COMMON_H__ */
--- subsys/net/lib/config/init_ieee802154.c
+++ subsys/net/lib/config/init_ieee802154.c
@@ -17,6 +17,74 @@ LOG_MODULE_DECLARE(net_config, CONFIG_NET_CONFIG_LOG_LEVEL);
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/ieee802154_mgmt.h>
 
+#include "init_common.h"
+#include "init_ieee802154.h"
+
+/* By simple encapsulation/information hiding principles, the radio driver should
+ * not know about L3+ (nor the other way round). We therefore do not want to mix
+ * up radio driver and interface settings in the same file! We can do that
+ * because no generator forces a structure on us.
+ *
+ * Note: Due to MAC offloading, the driver *could* know about offloaded parts of
+ * L2.
+ */
+struct net_config_ieee802154_radio_driver {
+	const struct device *dev;
+	const uint8_t ext_addr[IEEE802154_EXT_ADDR_LENGTH];
+	uint16_t pan_id;
+	uint16_t default_channel;
+#ifdef CONFIG_NET_L2_IEEE802154_SECURITY
+	const char sec_key[16];
+	uint8_t sec_key_mode;
+	uint8_t sec_level;
+#endif /* CONFIG_NET_L2_IEEE802154_SECURITY */
+	int16_t default_tx_pwr;
+	bool ack_required;
+};
+
+/* Normalization (and pragmatic driver pointer resolution) require us to attach
+ * driver hard and soft settings to the same abstract configuration entity. This
+ * does not violate encapsulation because hard and soft settings will be defined
+ * in different files (and even formats). Also soft settings will be determined
+ * relative to an interface configuration node while hard settings are relative
+ * to driver nodes with a given hardware compat.
+ *
+ * This is the reason why we cannot use inheritance to model a combination of
+ * hard and soft settings in a single binding hierarchy. We want them to be
+ * separate "mixins" contributing to the same driver node. That's why we compose
+ * two separate compats (each with its own binding) on one node:
+ * - one compat to determine the hardware-specific programming model (hard
+ *   settings) and
+ * - another compat to determine the hardware-independent driver programming
+ *   model (soft settings).
+ *
+ * NOTE: Both, hard and soft settings, are still radio *driver* settings in the
+ * most literal sense (see ieee802154_radio.h) and not interface settings. This
+ * is a good example why `struct driver` is not the correct way to distinguish
+ * between hard and soft settings.
+ */
+#define NET_CONFIG_IEEE802154_RADIO_DRIVER_STATE(_radio_driver_node_id, _ieee802154_state_name)    \
+	({                                                                                         \
+		 .dev = DEVICE_DT_GET(_radio_driver_node_id),                                      \
+		 .pan_id = DT_PROP(_radio_driver_node_id, pan_id),                                 \
+		 .ext_addr = DT_PROP(_radio_driver_node_id, ext_addr),                             \
+		 .default_channel = DT_PROP(_radio_driver_node_id, default_channel),               \
+		 .default_tx_pwr = DT_PROP(_radio_driver_node_id, default_tx_pwr),                 \
+		 .sec_key = DT_PROP(_radio_driver_node_id, sec_key),                               \
+		 .sec_key_mode = DT_PROP(_radio_driver_node_id, sec_key_mode),                     \
+		 .sec_level = DT_PROP(_radio_driver_node_id, sec_level),                           \
+		 .ack_required = !DT_PROP(_radio_driver_node_id, no_ack),                          \
+	},)
+
+#define NET_CONFIG_IEEE802154_RADIO_DRIVER(_iface_node_id)                                         \
+	IF_ENABLED(DT_NODE_HAS_COMPAT_STATUS(DT_PARENT(_iface_node_id),                            \
+					     zephyr_net_l2_ieee802154_native, okay),               \
+		   NET_CONFIG_IEEE802154_RADIO_DRIVER_STATE(                                       \
+			   DT_PARENT(_iface_node_id),                                              \
+			   NET_CONFIG_SUBSTATE_NAME(_iface_node_id, ieee802154)))
+
+/* TODO: instantiate config */
+
 int z_net_config_ieee802154_setup(struct net_if *iface)
 {
 	uint16_t channel = CONFIG_NET_CONFIG_IEEE802154_CHANNEL;
